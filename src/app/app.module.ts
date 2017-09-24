7a0d9c4b33ff36d4e066d111387094357e83d4c8
zhongchengli <lizhongcheng227@gmail.com>
2017-09-24 19:13:47 +1000

    Add Account and Accounts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { AccountComponent } from './account/account.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import {AccountService} from "./service/account.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    AccountComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
